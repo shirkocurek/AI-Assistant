93c51d0e339c6479acf3ab3ac5d31255c26274d1
Shir Kocurek <shir617@gmail.com>
2024-04-18 15:19:23 -0400

    added a function to get microphone premission from browser

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -1,9 +1,33 @@
-'use client'
+"use client";
 
 import Image from "next/image";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Recorder({uploadAudio}: { uploadAudio: (blob: Blob) => void}) {
+function Recorder({ uploadAudio }: { uploadAudio: (blob: Blob) => void }) {
+  
+    useEffect(() => {
+        getMicrophonePermission();
+    }, []);
+
+  const [permission, setPermission] = useState(false);
+  const [stream, setStream] = useState<MediaStream | null>(null);
+  const getMicrophonePermission = async () => {
+    if ("MediaRecorder" in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+        setPermission(true);
+        setStream(streamData);
+      } catch (err: any) {
+        alert(err.message);
+      }
+    } else {
+      alert("The MediaRecorder API is not supported in your browser.");
+    }
+  };
+
   return (
     <div className="flex flex-center justify-center text-white">
       <Image
