73260664f87c7b2419d23cb8d11b000af4daa324
Shir Kocurek <shirkocurek@Shirs-MBP.attlocal.net>
2024-04-18 10:13:38 -0400

    background color added

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import { SettingsIcon } from "lucide-react";
 import Image from "next/image";
+import Messages from "@/components/Messages";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="bg-black h-screen overflow-y-scroll-auto">
       <h1>Lets build an AI Assistant!</h1>
       {/* Header */}
-      <header
-        className="flex justify-between fixed top-0 text-white w-full p-5"
-      >
+      <header className="flex justify-between fixed top-0 text-white w-full p-5">
         <Image
           src="/Icon.jpeg"
           width={50}
@@ -22,6 +21,19 @@ export default function Home() {
         />
       </header>
       {/* Form */}
+      <form className="flex flex-col bg-black">
+        <div className="flex-1 bg-gradient-to-b from-purple-500 to-black">
+          {" "}
+          <Messages />{" "}
+        </div>
+        {/*Hidden fields*/}
+        <input type="file" />
+        <button type="submit" hidden />
+        <div className="fixed bottom-0 w-full overflow-hidden bg-black rounded-t-3xl">
+          {/*recorder*/}
+          <div>{/*voice synthesizer*/}</div>
+        </div>
+      </form>
     </main>
   );
 }
---
+++ components/Messages.tsx
@@ -0,0 +1,7 @@
+function Messages() {
+  return (
+    <div className="flex flex-col min-h-screen p-5 pt-20">Messages</div>
+  )
+}
+
+export default Messages
